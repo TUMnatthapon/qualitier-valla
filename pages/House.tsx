8f0b3d6a2e0e4a7a50f9b0e88e5ac10ed0d4afb3
TUMnatthapon <natthapon.tum01@gmail.com>
2022-08-24 21:03:24 +0700

    Update New

--- pages/House.tsx
+++ pages/House.tsx
@@ -1,4 +1,3 @@
-import Slide from "../components/Slide";
 import HouseIdea from "../components/houseIdea"
 import Tabs from "../components/Tabs";
 import Location from "../components/Location";
@@ -6,7 +5,6 @@ import Location from "../components/Location";
 const House = () => {
   return (
     <div>
-      <Slide />
       <HouseIdea />
       <Tabs />
       <Location />
